936c5731e92bd34ba82a8d3e5e791db70e5b87d3
KillahTw <134136307+Killahtw@users.noreply.github.com>
2023-10-21 10:41:24 +0300

    Add files via upload

---
+++ Transpositioncipher.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+std::string shift(const std::string& s, const std::string& key);
+
+int main() {
+    std::string text;
+    std::string key;
+    std::cout << "Enter your text:\n";
+    std::getline(std::cin, text);
+    std::cout << "Enter the key:\n";
+    std::getline(std::cin, key);
+    std::cout << "You text has been encrypt: " << shift(text, key)  << std::endl;
+    return 0;
+}
+
+std::vector<int> getArrayIndexes(const std::string& word) {
+    std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    std::vector<int> arrayIndexes;
+    for (char charInAlphabet : alphabet) {
+        for (int i = 0; i < word.length(); i++) {
+            if (charInAlphabet == word[i]) {
+                arrayIndexes.push_back(i);
+                break;
+            }
+        }
+    }
+    return arrayIndexes;
+}
+
+inline std::string shift(const std::string& s, const std::string& key) {
+    std::string line = s;
+    line.erase(std::remove(line.begin(), line.end(), ' '), line.end());
+    std::vector<std::vector<char>> matrix;
+    int index = 0;
+    for (int i = 0; i < (line.length() + key.length() - 1) / key.length(); i++) {
+        matrix.push_back(std::vector<char>());
+        while (index - i * key.length() < key.length()) {
+            if (index < line.length()) {
+                matrix[i].push_back(line[index]);
+            }
+            index++;
+        }
+    }
+
+    std::vector<int> indexes = getArrayIndexes(key);
+    std::string result = "";
+
+    for (int column : indexes) {
+        for (int i = 0; i < matrix.size(); i++) {
+            if (column < matrix[i].size()) {
+                result += matrix[i][column];
+            }
+        }
+    }
+    return result;
+}
---
+++ Vigenerecipher.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string vigenere_cipher(string text, string key) {
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    string encrypted_text = "";
+    int key_len = key.length();
+    int text_len = text.length();
+    for (int i = 0; i < text_len; i++) {
+        if (isalpha(text[i])) {
+            char lower_text_char = tolower(text[i]);
+            char lower_key_char = tolower(key[i % key_len]);
+            int char_index = alphabet.find(lower_text_char);
+            int key_index = alphabet.find(lower_key_char);
+            char encrypted_char = alphabet[(char_index + key_index) % alphabet.length()];
+            if (isupper(text[i])) {
+                encrypted_char = toupper(encrypted_char);
+            }
+
+            encrypted_text += encrypted_char;
+        }
+        else {
+            encrypted_text += text[i];
+        }
+    }
+
+    return encrypted_text;
+}
+
+int main() {
+    string key;
+    string netext;
+    cout << "Key: ";
+    cin >> key;
+    cout << "text: ";
+    cin.ignore();
+    getline(cin, netext);
+    string text = netext;
+    string encrypted_text = vigenere_cipher(text, key);
+    cout << "enc text::" << endl;
+    cout << encrypted_text << endl;
+    return 0;
+}
