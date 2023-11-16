011e7e063ad7fb27d9fab3f90c7bbc6687fd72e1
KillahTw <134136307+Killahtw@users.noreply.github.com>
2023-11-16 04:44:04 -0500

    Add files via upload

---
+++ rot.py
@@ -0,0 +1,7 @@
+def rot(s):
+    chars = "abcdefghijklmnopqrstuvwxyz"
+    for i in range(1, 26):
+	    trans = chars[i:]+chars[:i]
+	    rot_char = lambda c: trans[chars.find(c)] if chars.find(c)>-1 else c
+	    print( ''.join( rot_char(c) for c in s ) )
+rot("hello")
