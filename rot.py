c7ecf2c4cc3005f223667f085a5d460c8e056df3
KillahTw <134136307+Killahtw@users.noreply.github.com>
2023-11-17 08:45:18 +0300

    Update rot.py

--- rot.py
+++ rot.py
@@ -4,4 +4,3 @@ def rot(s):
 	    trans = chars[i:]+chars[:i]
 	    rot_char = lambda c: trans[chars.find(c)] if chars.find(c)>-1 else c
 	    print( ''.join( rot_char(c) for c in s ) )
-rot("hello")
